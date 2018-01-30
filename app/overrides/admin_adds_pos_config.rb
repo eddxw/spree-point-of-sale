31f9826dd3a44fdd758d4dc50dc277c6d10996f5
eddxw <eduardo.jemb@gmail.com>
2018-01-30 05:19:47 -0700

    deface add_adds_pos_config: insert_after: div.security -> insert_before: div.form-actions

--- app/overrides/admin_adds_pos_config.rb
+++ app/overrides/admin_adds_pos_config.rb
@@ -1,7 +1,7 @@
 Deface::Override.new(
  virtual_path: "spree/admin/general_settings/edit",
  name: "add_adds_pos_config",
- insert_after: "div.security",
+ insert_before: "div.form-actions",
  partial: "spree/admin/orders/admin_pos_config",
  disabled: false
 )
