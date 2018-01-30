f2baafd7b175ef00d20d4ff3193b71bd967da54a
eddxw <eduardo.jemb@gmail.com>
2018-01-30 03:43:36 -0700

    delegate_belongs_to -> delegate

--- lib/spree_pos/engine.rb
+++ lib/spree_pos/engine.rb
@@ -20,7 +20,7 @@ module SpreePos
         Rails.configuration.cache_classes ? require(c) : load(c)
       end
       Spree::Product.class_eval do
-        delegate_belongs_to :master, :ean
+        delegate :master, :ean
       end
     end
 
