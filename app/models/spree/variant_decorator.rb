dce7f07088de26b65138faf9668d78ec78ce1c78
eddxw <eduardo.jemb@gmail.com>
2018-04-10 04:10:31 -0700

    add ean

--- app/models/spree/variant_decorator.rb
+++ app/models/spree/variant_decorator.rb
@@ -1,5 +1,5 @@
 Spree::Variant.class_eval do
   scope :available_at_stock_location, ->(stock_location_id) { active.joins(:stock_items).where('spree_stock_items.count_on_hand > 0 AND spree_stock_items.stock_location_id = ?', stock_location_id)}
-  self.ransackable_attributes << 'ean'
+  self.whitelisted_ransackable_attributes << 'ean'
   self.whitelisted_ransackable_associations << 'variant_properties'
 end
